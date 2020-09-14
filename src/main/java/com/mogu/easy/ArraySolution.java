a949c6728971c49fb497e10286cd8a058965a9b8
weimj <weimj@mochasoft.com.cn>
2020-09-14 14:50:52 +0800

    删除排序数组中的重复项

--- src/main/java/com/mogu/easy/ArraySolution.java
+++ src/main/java/com/mogu/easy/ArraySolution.java
@@ -24,82 +24,14 @@ public class ArraySolution {
      * @return
      */
     public int removeDuplicates(int[] nums) {
-        int j = 0;
-        for (int i = 0; i < nums.length; i++) {
-            System.out.print("i=" + i + ",j=" + j + "=======");
-            if (nums[i] != nums[j]) {
-                j++;
-                nums[j] = nums[i];
+        int n = 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] != nums[n]){
+                n++;
+                nums[n] = nums[i];
             }
-            ArrayUtils.printArray(nums);
         }
         ArrayUtils.printArray(nums);
-        return j + 1;
-    }
-
-
-    /**
-     * 旋转数组
-     * https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/x2skh7/
-     *
-     * @param nums
-     * @param k
-     */
-    public void rotate(int[] nums, int k) {
-        int length = nums.length;
-        while (k > 0) {
-            int temp = nums[length - 1];
-            for (int i = length - 1; i > 0; i--) {
-                nums[i] = nums[i - 1];
-            }
-            nums[0] = temp;
-            ArrayUtils.printArray(nums);
-            k--;
-        }
-    }
-
-
-    /**
-     * 存在重复元素
-     * https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/x248f5/
-     *
-     * @param nums
-     * @return
-     */
-    public boolean containsDuplicate(int[] nums) {
-        HashSet set = new HashSet(nums.length);
-        for (int i = 0; i < nums.length; i++) {
-            if (set.contains(nums[i])) {
-                return true;
-            } else {
-                set.add(nums[i]);
-            }
-        }
-        return false;
-    }
-
-
-    /**
-     * 只出现一次的数字
-     * https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/x21ib6/
-     *
-     * @param nums
-     * @return
-     */
-    public int singleNumber(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (map.keySet().contains(nums[i])) {
-                map.put(nums[i], map.get(nums[i]) + 1);
-            } else {
-                map.put(nums[i], 1);
-            }
-        }
-        for (Map.Entry<Integer, Integer> e : map.entrySet()) {
-            if (e.getValue() == 1) {
-                return e.getKey();
-            }
-        }
-        return -1;
+        return n+1;
     }
 }
